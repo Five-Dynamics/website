3a9ac2c7d55af56109dd37f9f79dbfe0f40f7709
jaddenki <jaddenki@gmail.com>
2025-02-13 16:02:53 -0500

    header styling and file standards structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
+import styles from './style/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className={styles.textTopRight}>
+        <h2>ENGINEERING</h2>
+        <h2>THE</h2>
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              YO MAMA 
+              YO MAMA
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>hello</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -30,22 +27,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            whats up
           </a>
         </div>
       </main>
@@ -96,6 +78,10 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <div className={styles.textBottomLeft}>
+        <h2>EXTRA</h2>
+        <h2>ORDINARY</h2>
+      </div>
     </div>
   );
 }
